7e4aa16e7b4fcd8bfa0d9a4a0a083ea9c1dbadd3
Abhishek Shah <abhishekshah137@gmail.com>
2024-02-19 02:59:37 +0530

    streaming

--- spark-streaming.py
+++ spark-streaming.py
@@ -1,4 +1,5 @@
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import sum as _sum
 from pyspark.sql.functions import from_json, col
 from pyspark.sql.types import StructType, StructField, StringType, IntegerType, TimestampType
 
@@ -8,7 +9,7 @@ if __name__ == "__main__":
           .appName("Indian_Elections_2024")  # Application Name
           .master("local[*]") # Use local spark execution with all available cores
           .config("spark.jars.packages","org.apache.spark:spark-sql-kafka-0-10_2.13:3.5.0") # Spark-Kafka Integration
-          .config("spark.jars","C:\Users\abhis\Desktop\Voting System\postgresql-42.7.1.jar") # PostgreSQL Driver
+          .config("spark.jars", "/Users/abhis/Desktop/Voting_System/postgresql-42.7.1.jar") # PostgreSQL Driver
           .config("spark.sql.adaptive.enabled", "false") # Disable adaptive query execution
           .getOrCreate() 
           )
@@ -53,3 +54,35 @@ if __name__ == "__main__":
             .select(from_json(col("value"), vote_schema).alias("data")) 
             .select("data.*")
             )   
+  
+  # Pre-processing
+  votes_df = votes_df.withColumn('voting_time',col('voting_time').cast(TimestampType())) \
+    .withColumn('vote', col('vote').cast(IntegerType()))
+  enriched_votes_df = votes_df.withWatermark('voting_time','1 minute')
+
+  # Aggregate votes per candidate and turnout by location
+  votes_per_candidate = enriched_votes_df.groupBy('candidate_id','candidate_name', 'party_affiliation', 'photo_url').agg(_sum('vote').alias('total_votes'))
+  turnout_by_location = enriched_votes_df.groupBy('address.state').count().alias('total_votes')
+
+  # Write data back to kafka
+  votes_per_candidate_to_kafka = votes_per_candidate.selectExpr("to_json(struct(*)) AS value") \
+        .writeStream \
+        .format("kafka") \
+        .option("kafka.bootstrap.servers", "localhost:9092") \
+        .option("topic", "aggregated_votes_per_candidate") \
+        .option("checkpointLocation", "/Users/abhis/Desktop/Voting_System/checkpoints/checkpoint1") \
+        .outputMode("update") \
+        .start()
+
+  turnout_by_location_to_kafka = turnout_by_location.selectExpr("to_json(struct(*)) AS value") \
+        .writeStream \
+        .format("kafka") \
+        .option("kafka.bootstrap.servers", "localhost:9092") \
+        .option("topic", "aggregated_turnout_by_location") \
+        .option("checkpointLocation", "/Users/abhis/Desktop/Voting_System/checkpoints/checkpoint2") \
+        .outputMode("update") \
+        .start()
+
+  # Await termination for the streaming queries
+  votes_per_candidate_to_kafka.awaitTermination()
+  turnout_by_location_to_kafka.awaitTermination()
